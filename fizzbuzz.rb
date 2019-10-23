b6e20406d838e2e84199def0cc4e0f34e52b5f3c
Stela Capsa <stela.capsa@gmail.com>
2019-10-23 18:10:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 ==0
    # if the number int is divisible by 3
-     "Fizzbuzz" # Go fizz
+     "FizzBuzz" # Go fizz
 
 elsif int % 5 == 0 # if the number int is divisible by 3
    "Buzz" # Go fizz
