a22a87eb34e65e1d6b4f1fcc04f51318e3d4d17b
Stela Capsa <stela.capsa@gmail.com>
2019-10-23 17:20:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
 it 'returns "Fizz" when the number is divisible by 3' do
-  fizzbuzz(3) == "Fizz"  
+  fizzbuzz(3) == "Fizz"
   end
 end
